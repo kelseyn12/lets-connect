b3ceed970695f1ac4151d17a9f302a7366e55441
Kelseyn12 <kelseynocek12@gmail.com>
2025-10-06 13:38:34 -0500

    set up firebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ensureAnonymousAuth } from "./lib/firebase";
+
+ensureAnonymousAuth(); //  adds anonymous sign-in before render
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
